6eedc16c836271ae26d2b3fb74bd9563fb444a98
Шлома Сергей Николаевич <-@534school.spb.ru>
2024-02-16 15:44:42 +0300

    finished 3task on home task seminar 5

--- Seminar5_homeTask/src/Third.java
+++ Seminar5_homeTask/src/Third.java
@@ -34,14 +34,50 @@ import java.util.Arrays;
 class HeapSort {
 
     //buildTree, который строит сортирующее дерево на основе массива
-    public static void buildTree(int[] tree, int sortLength) {
+    public static void buildTree(int[] tree, int sortLength, int i) {
         // Введите свое решение ниже
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int l = 2*i + 1; // левый = 2*i + 1
+        int r = 2*i + 2; // правый = 2*i + 2
+
+        // Если левый дочерний элемент больше корня
+        if (l < sortLength && tree[l] > tree[largest])
+            largest = l;
+
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (r < sortLength && tree[r] > tree[largest])
+            largest = r;
+        // Если самый большой элемент не корень
+        if (largest != i)
+        {
+            int swap = tree[i];
+            tree[i] = tree[largest];
+            tree[largest] = swap;
+
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            buildTree(tree, sortLength, largest);
+        }
 
     }
 
     //heapSort, который выполняет собственно сортировку кучей
     public static void heapSort(int[] sortArray, int sortLength) {
-// Введите свое решение ниже
+        // Введите свое решение ниже
+        // Построение кучи (перегруппируем массив)
+        for (int i = sortLength / 2 - 1; i >= 0; i--)
+            buildTree(sortArray, sortLength, i);
+
+        // Один за другим извлекаем элементы из кучи
+        for (int i=sortLength-1; i>=0; i--)
+        {
+            // Перемещаем текущий корень в конец
+            int temp = sortArray[0];
+            sortArray[0] = sortArray[i];
+            sortArray[i] = temp;
+
+            // Вызываем процедуру heapify на уменьшенной куче
+            buildTree(sortArray, i, 0);
+        }
     }
 }
 
